0ec6d858dd10f87eb4bfa311500ee7fede2c86f0
justinksw <kongshuwa@gmail.com>
2023-01-10 16:08:39 +0800

    first commit

---
+++ kswutils/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='kswutils',
+      version='0.0',
+      description='Utils',
+      packages=['dist_alx'],
+      author_email='mihajlovic.aleksa@gmail.com',
+      zip_safe=False)
