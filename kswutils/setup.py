ec4282ca083c66241273fda098e7d4d8e28cf344
justinksw <kongshuwa@gmail.com>
2023-03-10 11:31:54 +0800

    add acc2

--- kswutils/setup.py
+++ kswutils/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kswutils',
-    version='0.2.0',
+    version='0.2.1',
     description='Justin utils at work',
     packages=['kswutils'],
     author_email='shuwa108@gmail.com',
